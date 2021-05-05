db968ff50faf016786ea192de7a6396362ab1cbd
halynas01 <galka.cv0108@gmail.com>
2021-05-05 18:42:54 -0500

    Initial commit

---
+++ src/test/java/com/cybertek/tests/day1_selenium_intro/BasicNavigations.java
@@ -0,0 +1,18 @@
+package com.cybertek.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigations {
+    public static void main(String[] args){
+//        1 - setup the browser driver
+        WebDriverManager.chromedriver().setup();
+//        2 - create instance of selenium web driver
+        WebDriver driver = new ChromeDriver();
+//        3 - get the page for Tesla.com
+        driver.get("https://www.tesla.com");
+
+
+    }
+}
